52bdfd78c3c49262222124296711799298616838
vsilva.wayon <vsilva.wayon@veloe.com.br>
2025-04-02 22:13:50 -0300

    configuracao inicial do portifolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContatoComponent } from './pages/contato/contato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjetosComponent } from './pages/projetos/projetos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'projetos', component: ProjetosComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ProjetosComponent } from './pages/projetos/projetos.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ProjetosComponent,
+    ContatoComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
